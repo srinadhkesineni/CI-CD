ee12e9fe64697811fe87a1946b9b39886418d931
Srinadh kesineni <srinadhkesineni7@gmail.com>
2025-06-17 10:40:20 +0530

    added env

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:18
-
-WORKDIR /app
-COPY package*.json ./
-RUN npm install        # cached if package.json didn’t change
-COPY . .               # only rest of files now
-CMD ["npm", "test"]
--- server/database/connectDB.js
+++ server/database/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/CICD_project");
+    await mongoose.connect(process.env.MONGO_URL);
 
     console.log("mongodb connection successfull!!");
   } catch (err) {
